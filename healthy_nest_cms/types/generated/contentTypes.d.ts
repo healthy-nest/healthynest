c622e0091b590e53afac48f42684633d57ba1529
Abhinav <Abhinavofficialmail@gmail.com>
2025-09-01 00:41:33 +0530

    Remove test fields

--- healthy_nest_cms/types/generated/contentTypes.d.ts
+++ healthy_nest_cms/types/generated/contentTypes.d.ts
@@ -586,7 +586,6 @@ export interface ApiElderNestElderNest extends Struct.CollectionTypeSchema {
       Schema.Attribute.Required;
     publishedAt: Schema.Attribute.DateTime;
     Specializations: Schema.Attribute.Component<'shared.catalog-item', true>;
-    TestField: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
